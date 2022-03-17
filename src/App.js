a004a502bbaf50ad50a3baed114a0632e62d025f
Evelyn Tan <evetan.wy@gmail.com>
2022-03-17 22:28:50 +1300

    added quest component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Questions from "./components/Questions";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>What is the Question Today</h1>
+      <Questions />
     </div>
   );
 }
---
+++ src/components/Questions.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Questions() {
+  return (
+    <div>
+      <h2>Test</h2>
+    </div>
+  );
+}
+
+export default Questions;
