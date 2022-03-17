f8d4de20ad872da94eac301ee9d091d9bc71f029
Evelyn Tan <evetan.wy@gmail.com>
2022-03-17 23:04:52 +1300

    handle click, to add json file of questions

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Questions() {
+  const intialState = {
+    question: "Why did the functional component feel lost?",
+    answer: "Because it didn't know what state it was in!",
+  };
+  const [question, setQuestion] = useState(intialState);
+
+  const handleClick = (e) => {
+    console.log(question);
+    setQuestion(question);
+  };
+
   return (
     <div>
       <h2>Test</h2>
+      <button onClick={handleClick}>Question</button>
+      {question.question}
     </div>
   );
 }
